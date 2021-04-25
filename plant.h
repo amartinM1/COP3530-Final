c8203142614ef43f6e0a6bc29acc3a0ea0991264
ChristinaKahn <82619594+ChristinaKahn@users.noreply.github.com>
2021-04-24 22:13:52 -0400

    Update plant.h
    Final Version

--- plant.h
+++ plant.h
@@ -1,3 +1,5 @@
+
+
 //
 // Created by alexi on 4/16/2021.
 //
@@ -10,45 +12,76 @@ using namespace std;
 class Plant {
 
 private:
-    string nickName=" ";
-    string Name=" ";
-    string notes=" ";
-    string states=" ";
+    string nickName = " ";
+    string Name = " ";
+    string notes = " ";
+    string states = " ";
+    //int val = 0;
+    
 public:
+    Plant();
     string GetnickName();
     string GetName();
     string Getnotes();
     string Getstates();
+   // int GetVal();
     void SetnickName(string nickName);
     void SetName(string Name);
     void Setnotes(string notes);
     void Setstates(string states);
+   // void SetVal(string name);
 
 };
-string Plant::GetnickName(){
+
+Plant::Plant() {
+    this->nickName = nickName;
+    this->Name = Name;
+    this->notes = notes;
+    this->states = states;
+}
+
+string Plant::GetnickName() {
 
     return nickName;
 }
-string Plant::GetName(){
+string Plant::GetName() {
     return Name;
 }
-string Plant::Getnotes(){
+string Plant::Getnotes() {
     return notes;
 }
-string Plant::Getstates(){
+string Plant::Getstates() {
     return states;
 }
-void Plant::SetnickName(string nickName){
-    this->nickName=nickName;
-}
-void Plant::SetName(string Name){
 
-    this->Name=Name;
+/*int Plant::GetVal() {
+   
+    if (val == NULL) {
+
+        return 0;
+    }
+    return val;
+}*/
+void Plant::SetnickName(string nickName) {
+    this->nickName = nickName;
+}
+void Plant::SetName(string Name) {
+
+    this->Name = Name;
 }
 void Plant::Setnotes(string notes)
 {
-    this->notes=notes;
+    this->notes = notes;
 }
-void Plant::Setstates(string states){
-    this->states=states;
+void Plant::Setstates(string states) {
+    this->states = states;
 }
+
+/*void Plant::SetVal(string name) {
+   // this->name = name;
+    int total = 0;
+    for (int i = 0; i < name.length(); i++) {
+        total += int(name[i]);
+    }
+    this->val = total;
+}*/
